fddbe5c6f3ad6c7683ed91b362e3c466f32d8e0d
TusharSatam <tsatam91@gmail.com>
2024-07-23 20:59:32 +0530

    Initial project setup - Added assets file, css variables

---
+++ assets/Images/index.js
@@ -0,0 +1,27 @@
+export { default as LOGO } from "./LOGO.svg";
+export { default as backgroundLayer } from "./backgroundLayer.svg";
+
+// Icons
+export { default as chatIcon } from "./Icons/chatIcon.svg";
+export { default as defaultProfileBlackIcon } from "./Icons/defaultProfileBlackIcon.svg";
+export { default as defaultProfileIcon } from "./Icons/defaultProfileIcon.svg";
+export { default as deutschFlagIcon } from "./Icons/deutschFlagIcon.svg";
+export { default as expandDownIcon } from "./Icons/expandDownIcon.svg";
+export { default as fagIcon } from "./Icons/fagIcon.svg";
+export { default as hambargerIcon } from "./Icons/hambargerIcon.svg";
+export { default as moboHambargerIcon } from "./Icons/moboHambargerIcon.svg";
+export { default as helpIcon } from "./Icons/helpIcon.svg";
+export { default as navigateIcon } from "./Icons/navigateIcon.svg";
+export { default as sendQueryIcon } from "./Icons/sendQueryIcon.svg";
+export { default as socialIcon } from "./Icons/socialIcon.svg";
+export { default as travelGuideIcon } from "./Icons/travelGuideIcon.svg";
+export { default as upArrowIcon } from "./Icons/upArrowIcon.svg";
+// SocialMedia Icons
+export { default as facebook } from "./Icons/SocialMediaIcons/facebook.svg";
+export { default as instagram } from "./Icons/SocialMediaIcons./instagram.svg";
+export { default as linkedIn } from "./Icons/SocialMediaIcons./linkedIn.svg";
+export { default as liveChat } from "./Icons/SocialMediaIcons./liveChat.svg";
+export { default as whatsApp } from "./Icons/SocialMediaIcons./whatsApp.svg";
+export { default as telegram } from "./Icons/SocialMediaIcons./telegram.svg";
+export { default as twitter } from "./Icons/SocialMediaIcons./twitter.svg";
+
